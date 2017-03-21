e2e48881373184616776355956d564f0cd444eaf
silvia-fang <danqif@upenn.edu>
2017-03-21 18:28:22 -0400

    Wk5_Fang
    assignment

--- assignment/js/main.js
+++ assignment/js/main.js
@@ -1,3 +1,53 @@
+//downloadData
+var downloadData = $.ajax("https://raw.githubusercontent.com/CPLN690-MUSA610/datasets/master/json/philadelphia-bike-crashes-snippet.json");
+console.log(downloadData);
+
+//parse data
+
+var parseData = function(data) {
+  parsed = JSON.parse(data);
+  return parsed;
+};
+
+//make markers
+
+var makeMarkers = function(parsed) {
+  markerDots = _.map(parsed, function(obj){
+    return L.marker([obj.LAT, obj.LNG]);
+  });
+  return markerDots;
+};
+
+//add marker
+var plotMarkers = function(markerDots) {
+  _.each(markerDots, function(pin) {
+    pin.addTo(map);
+  });
+};
+
+
 /* =====================
- Copy your code from Week 4 Lab 2 Part 2 part2-app-state.js in this space
+ Leaflet setup - feel free to ignore this
 ===================== */
+
+var map = L.map('map', {
+  center: [39.9522, -75.1639],
+  zoom: 14
+});
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+/* =====================
+ CODE EXECUTED HERE!
+===================== */
+
+downloadData.done(function(data) {
+  var parsed = parseData(data);
+  var markers = makeMarkers(parsed);
+  plotMarkers(markers);
+  });
